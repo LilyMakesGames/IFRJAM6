79f4e93099e14a4296316930fc2d1c0a632fcd0c
Raphael <raphael.rodrigues.macedo@gmail.com>
2019-03-30 20:46:42 -0300

    Script do painel Controle da Animação Animação andando para baixo, par cima e para o lado (sprite base)

---
+++ Assets/Scripts/Canvas.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas : MonoBehaviour
+{
+	Animator animator;
+	bool isOpen = true;
+	bool isAlertOn = false;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("tab"))
+		{
+			if (isOpen)
+			{
+				closePanel();
+			}
+			else
+			{
+				openPanel();
+			}
+		}
+		if (Input.GetKeyDown("q"))
+		{
+			activeAlert();
+		}
+    }
+	
+	void closePanel()
+	{
+		animator.SetTrigger("closeTrigger");
+		isOpen = false;
+	}
+	
+	void openPanel()
+	{
+		animator.SetTrigger("openTrigger");
+		animator.SetBool("isAlertOn", false);
+		isOpen = true;
+	}
+	
+	void activeAlert()
+	{
+		if (!isOpen)
+		{
+			animator.SetBool("isAlertOn", true);
+		}
+	}
+	
+	
+}
